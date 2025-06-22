7467d74717d3a50a201563bf14990a3c6d6da9a2
Garvit <garvittyagicoe@gmail.com>
2025-06-22 15:11:10 +0530

    fix card 4

--- src/components/StaticPages/CardSection4.tsx
+++ src/components/StaticPages/CardSection4.tsx
@@ -3,12 +3,16 @@ import Card from "@/components/StaticPages/Card";
 
 const CardSection4 = ({ data }: { data: CardSection }) => {
   return (
-    <section className="px-20 py-16">
-      <h2 className="text-4xl font-bold mb-2">{data.title}</h2>
+    <section className="px-[120px] py-16">
+      <h2 className="w-full max-w-[659px] h-auto min-h-[112px] text-3xl md:text-[48px] font-semibold leading-tight md:leading-[56px] tracking-[-1px] md:tracking-[-2px] text-center text-white flex items-center justify-center px-4 justify-self-center mb-4 mt-[120px]">
+        {data.title}
+      </h2>
       {data.description && (
-        <p className="text-lg text-white/70 mb-8">{data.description}</p>
+        <p className="w-full max-w-[703px] h-auto min-h-[20px] text-base font-light leading-5 tracking-[-0.16px] text-center text-[#8A8A8A] mx-auto mb-[68px] justify-self-center">
+        {data.description}
+      </p>
       )}
-      <div className="grid grid-cols-2 grid-rows-2 gap-8">
+      <div className="grid grid-cols-2 grid-rows-2 gap-[40px]">
         {data.cardSection.map((card, idx) => (
           <Card data={card} key={idx} />
         ))}
