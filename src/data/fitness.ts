ce4f4cf02c28561220bf022ec59068e908e1102b
Garvit <garvittyagicoe@gmail.com>
2025-07-01 14:37:40 +0530

    text updated

--- src/data/fitness.ts
+++ src/data/fitness.ts
@@ -15,45 +15,242 @@ export const fitnessData: StaticPageData = {
     btnPrimaryText: "Book a free trial",
     btnSecondaryText: "Join Online",
   },
-  cardSection4: {
-    title: "Fitness for Every Body",
-    description: "Step into a world of advanced fitness and training.",
+  cardSection5: {
+    title: "The ultimate fitness experience",
+    description: "",
     cardSection: [
       {
         title: "Advanced 3D Body Scan",
         description:
           "Launch your fitness journey with actionable insights for best performance.",
-        backgroundImage: "/images/card-section/fitness/body-scan.svg",
+        backgroundImage: "/images/cricket/cricket-1.svg",
+        iconImage: "/images/cricket/cricket-icon-1.svg",
         link: "",
       },
       {
-        title: "Personal Training",
-        description:
-          "Customized 1-on-1 coaching tailored to your body, goals, sport-specific performance—and complete recovery needs.",
-        backgroundImage: "/images/card-section/fitness/personal-training.svg",
-        link: "",
-      },
-      {
-        title: "Group Classes",
-        description:
-          "Our expert-led group sessions fuel your energy and connect you with a community that shares your drive.",
-        backgroundImage: "/images/card-section/fitness/group-classes.svg",
+        title: "Group classes",
+        description: "Our expert-led group sessions fuel your energy and connect you with a community that shares your drive.",
+        backgroundImage: "/images/cricket/cricket-5.svg",
+        iconImage: "/images/cricket/cricket-icon-2.svg",
         link: "",
       },
       {
         title: "Hyrox-Inspired Zone",
-        description:
-          "Experience the energy, structure, and challenge of the global fitness race at your club.",
-        backgroundImage: "/images/card-section/fitness/hyrox.svg",
+        description: "Experience the energy, structure, and challenge of the global fitness race at your club",
+        backgroundImage: "/images/cricket/cricket-3.svg",
+        iconImage: "/images/cricket/cricket-icon-3.svg",
         link: "",
       },
       {
-        title: "High and Low Intensity workout zones",
-        description:
-          "From sweat-drenched HIIT sessions to mindful low-impact training, our zones allow you to train full spectrum.",
-        backgroundImage: "/images/card-section/fitness/intensity-zones.svg",
+        title: "Personal training",
+        description: "Customized 1-on-1 coaching tailored to your body, goals, sport-specific performance—and complete recovery needs.",
+        backgroundImage: "/images/cricket/cricket-1.svg",
+        iconImage: "/images/cricket/cricket-icon-4.svg",
+        link: "",
+      },
+      {
+        title: "High and Low Intensity workout zones ",
+        description: "From sweat-drenched HIIT sessions to mindful low-impact training, our zones allow you to train full spectrum",
+        backgroundImage: "/images/cricket/cricket-5.svg",
+        iconImage: "/images/cricket/cricket-icon-5.svg",
         link: "",
       },
     ],
+  },
+  evolutionSection: {
+    title: "Discover a new era of fitness",
+    evolutionList: [
+      {
+        title: "Best fitness socials",
+        description:
+          "Part fitness class, part fitness party. Experience heart-pumping music, and a group atmosphere that makes sweating feel like celebrating.",
+        backgroundImage: "/images/cricket/evolution-1.svg",
+      },
+      {
+        title: "Mind body integration",
+        description:
+          "Experience programs that combine physical movement with mental well-being, such as yoga, and guided breathwork",
+        backgroundImage: "/images/fitness/evolution-1.svg",
+      },
+      {
+        title: "Indoor running studio",
+        description: "Join the pack. Run with purpose at Drive FIIT. Our program helps you build proper technique to help you excel across all race formats.",
+        backgroundImage: "/images/recovery/evolution-1.svg",
+      },
+    ],
+  },
+  gallerySection: {
+    title: "Fitness unfiltered",
+    description:
+      "Where every drop of sweat writes your success story",
+    btnLabel: "View Gallery",
+    imageList: [
+      "/images/gallery-section/gallery-cricket-1.svg",
+      "/images/gallery-section/gallery-cricket-2.svg",
+      "/images/gallery-section/gallery-cricket-3.svg",
+    ],
+  },
+  memberSection: {
+    title: "Member experiences",
+    description:
+      "Hear What Our Players Say — Real Stories of Growth, Grit, and Game-Changing Training.",
+    memberList: [
+      {
+        title: "Kunal Bajaj",
+        description: "Membership plan",
+        backgroundImage: "/images/member-section/cricket-member-1.svg",
+        link: "/cricket",
+      },
+      {
+        title: "Kunal Jain",
+        description: "Membership plan",
+        backgroundImage: "/images/member-section/cricket-member-2.svg",
+        link: "/cricket",
+      },
+      {
+        title: "Rahul Kapoor",
+        description: "Membership plan",
+        backgroundImage: "/images/member-section/cricket-member-3.svg",
+        link: "/cricket",
+      },
+    ],
+  },
+  faqSection: {
+    title: "Frequently Asked Questions",
+    description: "Everything you need to know about the Drivefitt and billing.",
+    faqList: [
+      {
+        title: "How is DRIVE different from regular cricket academies?",
+        description:
+          "Yes, you can try us for free for 30 days. If you want, we'll provide you with a free, personalized 30-minute onboarding call to get you up and running as soon as possible.",
+      },
+      {
+        title: "Do I Need Prior Experience To Join?",
+        description:
+          "No prior cricket experience is required. Our program is designed to accommodate players of all skill levels, from beginners to advanced cricketers. Our personalized approach ensures that you'll receive training tailored to your current ability and goals.",
+      },
+      {
+        title: "What's Included In The Membership?",
+        description:
+          "Our membership includes comprehensive cricket training, personalized coaching, access to state-of-the-art facilities, performance tracking, video analysis, and regular progress assessments. You'll also get access to our online resources and community of players.",
+      },
+      {
+        title: "Can My Fitness Trainer Access My Cricket Data?",
+        description:
+          "Yes, we provide integration options that allow your fitness trainer to access your cricket performance data. This helps create a more holistic training approach by combining cricket-specific skills with overall fitness development.",
+      },
+    ],
+  },
+  appDownloadSection: {
+    title: "Your exclusive access, now on mobile",
+    description:
+      "Our app makes it easy to join classes, connect with coaches and track your journey.",
+    googlePlayImg: "/images/app-download/google-play.svg",
+    appStoreImg: "/images/app-download/app-store.svg",
+    dfLogo: "/images/app-download/df-logo.svg",
+    mobileImage: "/images/app-download/mobile.png",
+  },
+  footerInfoSection: {
+    footerInfoList: [
+      {
+        title: "Chat to us",
+        description: "Our friendly team is here to help",
+        email: "info@drivefitt.club",
+        image: "/images/ChatToUs.svg",
+      },
+      {
+        title: "Visit us",
+        description: "Welcome to Our Main Club Branch.",
+        email: "Sector-56, Golf Course RdGurugram, Haryana 122022",
+        image: "/images/VisitUs.svg",
+      },
+      {
+        title: "Call us",
+        description: "Mon - Sun from 10am to 10pm",
+        email: "+91-9999999990",
+        image: "/images/CallUs.svg",
+      },
+    ],
+    socialLinkList: [
+      {
+        image: "/images/x-social.svg",
+        link: "",
+      },
+      {
+        image: "/images/instagram-social.svg",
+        link: "",
+      },
+      {
+        image: "/images/linkedin-social.svg",
+        link: "",
+      },
+      {
+        image: "/images/facebook-social.svg",
+        link: "",
+      },
+    ],
+    contactFormSection: {
+      title: "Get in touch",
+      description: "We'd love to hear from you. Please fill out this form.",
+      firstName: "",
+      lastName: "",
+      email: "",
+      phone: "",
+      message: "",
+      submitButtonText: "Send Message",
+    },
+  },
+  footerSection: {
+    logo: "/images/logo.svg",
+    description:
+      "Experience Gurugram's premier fitness & sports club – Gym, cricket, recovery & more",
+    sections: [
+      {
+        title: "Quick links",
+        links: [
+          { title: "About us", link: "/about" },
+          { title: "Our services", link: "/services" },
+          { title: "Blogs", link: "/blogs" },
+          { title: "Career", link: "/career" },
+          { title: "Partner with us", link: "/partner" },
+        ],
+      },
+      {
+        title: "Services",
+        links: [
+          { title: "Cricket", link: "/cricket" },
+          { title: "Fitness", link: "/fitness" },
+          { title: "Recovery", link: "/recovery" },
+          { title: "Running", link: "/running" },
+          { title: "Group Classes", link: "/group-classes" },
+          { title: "Pilates", link: "/pilates" },
+          { title: "Personal Training", link: "/personal-training" },
+        ],
+      },
+      {
+        title: "Support",
+        links: [
+          { title: "Account", link: "/account" },
+          { title: "Help", link: "/help" },
+          { title: "Contact Us", link: "/contact" },
+          { title: "Customer Support", link: "/support" },
+        ],
+      },
+      {
+        title: "Legals",
+        links: [
+          { title: "Terms & Conditions", link: "/terms" },
+          { title: "Privacy & Policy", link: "/privacy" },
+          { title: "Licenses", link: "/licenses" },
+        ],
+      },
+    ],
+    socialLinks: [
+      { image: "/images/x-social.svg", link: "" },
+      { image: "/images/instagram-social.svg", link: "" },
+      { image: "/images/linkedin-social.svg", link: "" },
+      { image: "/images/facebook-social.svg", link: "" },
+    ],
+    copyright: "©2025 Drivefitt. All rights reserved.",
   },
 };
