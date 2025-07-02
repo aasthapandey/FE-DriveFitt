f65ffa566747fd0b3ef6dfcd5629971f42c07e6c
Garvit <garvittyagicoe@gmail.com>
2025-07-02 09:42:37 +0530

    mobile css added in comments

--- src/components/StaticPages/Error404Section.tsx
+++ src/components/StaticPages/Error404Section.tsx
@@ -29,17 +29,19 @@ const Error404Section = ({ data, isMobile }: Error404SectionProps) => {
               className="opacity-80"   
             />
           </div>
-          
+          {/*md:max-w-[240px] md:max-h-[28px] md:font-semibold md:text-[24px] md:leading-[28px] md:tracking-[-1px] */}
           <h1 className="text-[#FFFFF7] max-w-[479px] max-h-[60px] font-semibold text-[48px] leading-[60px] tracking-[-2px] text-center mb-[24px]">
             {title}
           </h1>
         
+          {/* md:max-w-[364px] md:max-h-[16px] md:text-[12px] md:leading-[16px] */}
           <p className="max-w-[262px] max-h-[24px] font-light text-[16px] leading-[24px] tracking-[-1px] text-center mb-[48px]">
             {description}
           </p>
-
+          
+          {/* md:max-w-[108px] md:max-h-[20px] md:text-[14px] md:leading-[20px]  */}
           <Link href={btnLink}>
-            <button className="bg-[#00DBDC] w-[187px] h-[56px] rounded-[8px] py-4 gap-[10px]font-medium text-[20px] leading-[100%] tracking-[-2%] text-black">
+            <button className="bg-[#00DBDC] w-[187px] h-[56px] rounded-[8px] py-4 gap-[10px] font-medium text-[20px] leading-[100%] tracking-[-2%] text-black">
               {btnText}
             </button>
           </Link>
