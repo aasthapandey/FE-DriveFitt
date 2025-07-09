1f8769f95b76213039ffb9abe7e56d69f3963d91
Garvit <garvittyagicoe@gmail.com>
2025-07-09 21:58:40 +0530

    added get and set APIs for data file update

--- src/app/franchise/page.tsx
+++ src/app/franchise/page.tsx
@@ -1,16 +1,13 @@
 import { Metadata } from "next";
 import { franchiseData } from "@/data/franchise";
-import { StaticPageData } from "@/types/staticPages";
 import FranchisePage from "@/components/FranchisePage";
 import { headers } from "next/headers";
 import { isMobileDevice } from "@/utils/deviceDetection";
 
-const pageData: StaticPageData = franchiseData;
-
 export async function generateMetadata(): Promise<Metadata> {
   return {
-    title: pageData.seoTitle || pageData.title,
-    description: pageData.seoDescription,
+    title: franchiseData.seoTitle || franchiseData.title,
+    description: franchiseData.seoDescription,
   };
 }
 
