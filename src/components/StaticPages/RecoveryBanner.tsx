adafb6289fde0702a9a3f2a3cfa9810789452e40
Garvit <garvittyagicoe@gmail.com>
2025-07-14 01:33:20 +0530

    new component added, removed recovery page component

--- src/components/StaticPages/RecoveryBanner.tsx
+++ src/components/StaticPages/RecoveryBanner.tsx
@@ -10,7 +10,7 @@ const RecoveryBanner = ({ data, isMobile }: RecoveryBannerType) => {
   const imageToUse = isMobile && mobileImage ? mobileImage : image;
 
   return (
-    <section className="md:px-[120px] px-6 flex flex-col gap-5 md:gap-8">
+    <section className="md:px-[120px] px-6 flex flex-col gap-5 ">
       <div
         className="rounded-[20px] md:rounded-[40px] p-[2px] h-[224px] md:h-[408px]"
         style={{
@@ -19,7 +19,7 @@ const RecoveryBanner = ({ data, isMobile }: RecoveryBannerType) => {
         }}
       >
         <div
-          className="rounded-[20px] md:rounded-[40px] w-full h-full flex flex-col justify-center p-6 md:p-10"
+          className="rounded-[20px] md:rounded-[40px] w-full h-full flex flex-col justify-center p-6"
           style={{
             background: `linear-gradient(180deg, rgba(13, 13, 13, 0) 0%, #0D0D0D 100%), url(${imageToUse})`,
             backgroundRepeat: "no-repeat",
@@ -28,7 +28,7 @@ const RecoveryBanner = ({ data, isMobile }: RecoveryBannerType) => {
             backgroundSize: "cover",
           }}
         >
-          <div className="flex flex-col justify-center h-full items-start max-w-[600px] md:pl-[60px]">
+          <div className="flex flex-col h-full items-start max-w-[600px] md:pl-[36px] md:pt-[36px] md:pb-[36px]">
             <h2 className="text-white text-2xl md:text-[48px] md:leading-[66px] font-semibold tracking-[-2.4px] mb-4 md:mb-[28px] md:max-w-[441px] capitalize">
               {title}
             </h2>
