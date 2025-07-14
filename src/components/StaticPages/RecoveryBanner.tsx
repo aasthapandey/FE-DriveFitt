11f3db8766fe01090525d732af70b00365e70df3
Garvit <garvittyagicoe@gmail.com>
2025-07-14 12:11:25 +0530

    recovery banner, mobile view

--- src/components/StaticPages/RecoveryBanner.tsx
+++ src/components/StaticPages/RecoveryBanner.tsx
@@ -10,9 +10,9 @@ const RecoveryBanner = ({ data, isMobile }: RecoveryBannerType) => {
   const imageToUse = isMobile && mobileImage ? mobileImage : image;
 
   return (
-    <section className="md:px-[120px] px-6 flex flex-col gap-5 ">
+    <section className="md:px-[120px] px-6 flex flex-col gap-5">
       <div
-        className="rounded-[20px] md:rounded-[40px] p-[2px] h-[224px] md:h-[408px]"
+        className="rounded-[20px] md:rounded-[40px] p-[1.5px] md:p-[2px] h-[392px] md:h-[408px md:w-full mx-auto"
         style={{
           background:
             "linear-gradient(180deg, #333333 29.36%, #00DBDC 120.13%)",
@@ -28,11 +28,11 @@ const RecoveryBanner = ({ data, isMobile }: RecoveryBannerType) => {
             backgroundSize: "cover",
           }}
         >
-          <div className="flex flex-col h-full items-start max-w-[600px] md:pl-[36px] md:pt-[36px] md:pb-[36px]">
-            <h2 className="text-white text-2xl md:text-[48px] md:leading-[66px] font-semibold tracking-[-2.4px] mb-4 md:mb-[28px] md:max-w-[441px] capitalize">
+          <div className="flex flex-col h-full items-center justify-end md:items-start md:max-w-[600px] md:pl-[36px] md:pt-[36px] md:pb-[36px]">
+            <h2 className="text-white text-[24px] md:text-[48px] leading-[28px] md:leading-[66px] font-semibold tracking-[-1px] md:tracking-[-2.4px] mb-4 md:mb-[28px] md:max-w-[441px] capitalize text-center md:text-left">
               {title}
             </h2>
-            <p className="text-white text-[16px] md:text-[16px] leading-[18px] md:leading-[32px] font-light tracking-[0px] md:max-w-[536px]">
+            <p className="text-white text-[12px] md:text-[16px] leading-[20px] md:leading-[32px] font-light tracking-[0px] md:max-w-[536px] text-center md:text-left">
               {description}
             </p>
           </div>
