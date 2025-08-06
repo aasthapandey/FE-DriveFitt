85d6bac7c92ef2fead5d4a56ed5f2a639b54cece
sahilduatechkat <sahyldua@gmail.com>
2025-08-06 11:50:42 +0530

    Update home.ts
    text changes

--- src/data/home.ts
+++ src/data/home.ts
@@ -258,7 +258,7 @@ export const homeData: StaticPageData = {
       {
         title: "Visit Us",
         description:
-          "Discover the best of what we i offer at our Flagship Club",
+          "Discover the best of what we offer at our Flagship Club",
         email: "M3M 65th Avenue, Sector 65, Gurugram, Haryana 122022",
         image: "https://da8nru77lsio9.cloudfront.net/images/VisitUs.svg",
       },
@@ -292,7 +292,7 @@ export const homeData: StaticPageData = {
     ],
     contactFormSection: {
       title: "Get In Touch",
-      description: "We'd Love To Hear From You. Please Fill Out This Form.",
+      description: "We'd love to hear from you. Please fill out this form.",
       submitButtonText: "Send Message",
       fields: {
         firstName: {
