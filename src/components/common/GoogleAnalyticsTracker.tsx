9dd36f6696d3b04a6f56fc1d2dd5ae09e9baaa9f
unknown <garvittyagicoe@gmail.com>
2025-08-11 22:45:35 +0530

    google analytics flow added

--- src/components/common/GoogleAnalyticsTracker.tsx
+++ src/components/common/GoogleAnalyticsTracker.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, Suspense } from "react";
 import { usePathname, useSearchParams } from "next/navigation";
 import { pageview } from "@/lib/gtag";
 
-export default function GoogleAnalyticsTracker() {
+function GoogleAnalyticsTrackerInner() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
@@ -14,4 +14,12 @@ export default function GoogleAnalyticsTracker() {
   }, [pathname, searchParams]);
 
   return null;
+}
+
+export default function GoogleAnalyticsTracker() {
+  return (
+    <Suspense fallback={null}>
+      <GoogleAnalyticsTrackerInner />
+    </Suspense>
+  );
 }
